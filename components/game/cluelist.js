8a59d109006ede6b8930f71439825906f851d51a
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-08 16:57:26 +0100

    Clues added

---
+++ components/game/cluelist.js
@@ -0,0 +1,21 @@
+export default function ClueList(props) {
+  const clues = props.clues
+  const cluesList = clues.map((clue) => {
+   return <li key= {clues.id}>
+    {clue}
+  </li>
+  })
+  return (
+    <ul>
+      {cluesList}
+    </ul>
+  )
+}
+
+    // return (
+    //   <li>
+    //     clue
+    //   </li>
+    // );
+    // })
+ 
--- pages/game.js
+++ pages/game.js
@@ -1,11 +1,16 @@
 import GameHeader from '../components/game/gameHeader.js'
 import Map from '../components/game/map.js'
+import ClueList from '../components/game/clueList.js'
 
+const props = ["This country has many states", "Where is Disneland's home", "It has the nickname 'Magic City'"]
+
+console.log(props);
 export default function Game() {
   return (
     <div style={{width: "1400px", height: "1000px", borderStyle: "double"}}>
       <GameHeader />
       <Map />
+      <ClueList clues = {props} />
     </div>
   )
 }
