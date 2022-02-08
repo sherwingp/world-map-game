85f2314f017ee3732bb35e64c4d4b5f296d5d056
Fadaka <master_kunle14@hotmail.co.uk>
2022-02-08 17:02:29 +0000

    add clue component

---
+++ components/game/clue.js
@@ -0,0 +1,8 @@
+export default function Clue(text) {
+    return (
+    <li className= "clue"> 
+        {text}
+    </li>
+    )
+}
+    
--- components/game/cluelist.js
+++ components/game/cluelist.js
@@ -1,10 +1,16 @@
+import Clue from "./clue.js"
 export default function ClueList(props) {
   const clues = props.clues
-  const cluesList = clues.map((clue) => {
-   return <li key= {clues.id}>
-    {clue}
-  </li>
-  })
+  const cluesList = clues.map((clue) => (
+    <Clue
+        id = {clue.id}
+        text = {clue.text}
+        key = {clue.id}
+    />
+  )
+  )
+  console.log(cluesList)
+
   return (
     <ul>
       {cluesList}
