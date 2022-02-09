4c701e45d9a5b9253fd3142d6adf35b59b6f1b8c
zidehen <zoeidehen@gmail.com>
2022-02-09 11:43:27 +0000

    Started welcome page feature test

---
+++ cypress/integration/welcome.spec.js
@@ -0,0 +1,13 @@
+describe('Welcome page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+    
+  it('enters a player name', () => {
+    cy.get('input').type('Zoe')
+        
+    cy.get('button[type="submit"]')
+    .should('be.visible')
+    .click()
+  })
+})
