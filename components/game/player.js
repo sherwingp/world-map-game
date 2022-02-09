3f85aaea3ebf2f030cd4c8f803e4855582712b21
zidehen <zoeidehen@gmail.com>
2022-02-09 20:47:41 +0000

    Added player component

---
+++ components/game/player.js
@@ -0,0 +1,13 @@
+// import { useState } from "react"
+// import Form from '../components/Form'
+// const [player, setPlayer] = useState({name})
+
+import { name } from '../components/Form'
+
+const Player = ({ name }) => {
+    return <li className="player">{name}</li>;
+    }
+    
+ 
+   export default Player;
+    
