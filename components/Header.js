e7f4a639ab10e0a4cb78746eb00899fe64662e00
kehinde alaka <kennyalaka93@gmail.com>
2022-02-09 13:15:51 +0000

    unit test for header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return ( 
         <div className="header">
-           <h1>Welcome to MAP-PIN!</h1>
+           <h1 data-testid="header-content">Welcome to MAP-PIN!</h1>
         </div>
      );
 }
