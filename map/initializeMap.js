811ab6bac0453ca40333731312f8224cb877cdcd
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-02-13 13:40:35 +0000

    Refactor add_marker to prevent state mutation

--- map/initializeMap.js
+++ map/initializeMap.js
@@ -2,8 +2,8 @@ export function initializeMap(mapboxgl, map, setMessage, location, setLocation) 
   const marker = new mapboxgl.Marker();
 
   function add_marker(event) {
-    location = event.lngLat;
-    setLocation(location)
+    const clickedLocation = event.lngLat;
+    setLocation(clickedLocation)
     console.log("Lng:", location.lng, "Lat:", location.lat);
     marker.setLngLat(location).addTo(map);
     setMessage("");
@@ -11,13 +11,3 @@ export function initializeMap(mapboxgl, map, setMessage, location, setLocation) 
 
   map.on("click", add_marker);
 }
-
-//  this is what it was before
-
-// function add_marker(event) {
-//   var coordinates = event.lngLat;
-//   setLocation(coordinates)
-//   console.log("Lng:", location.lng, "Lat:", location.lat);
-//   marker.setLngLat(coordinates).addTo(map);
-//   setMessage("");
-// }
